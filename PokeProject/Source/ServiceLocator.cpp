e68fff255835638c67204f74cc6261da6c84875e
Mauro Deryckere <92228779+MauroDeryckere@users.noreply.github.com>
2024-11-04 23:26:15 +0100

    Delete PokeProject directory

--- PokeProject/Source/ServiceLocator.cpp
+++
@@ -1,8 +0,0 @@
-#include "ServiceLocator.h"
-
-//Default intitialize systems in the service locator
-
-namespace PokeProject
-{
-	std::unique_ptr<PokeProject::JsonParser> ServiceLocator::m_pJsonParser{ std::make_unique<NullJsonParser>() };
-}
